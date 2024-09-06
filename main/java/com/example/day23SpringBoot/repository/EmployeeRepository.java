989ff23f2fd9e3fa35026242a36014a357ec64ab
Akhilesh Maurya <Akhilesh.Maurya@godigit.com>
2024-09-06 13:37:34 +0530

    Second commit with model and Repository

--- main/java/com/example/day23SpringBoot/repository/EmployeeRepository.java
+++ main/java/com/example/day23SpringBoot/repository/EmployeeRepository.java
@@ -1,5 +1,10 @@
 package com.example.day23SpringBoot.repository;
 
-public interface EmployeeRepository  {
+import com.example.day23SpringBoot.model.EmployeeModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<EmployeeModel , Long> {
 
 }
