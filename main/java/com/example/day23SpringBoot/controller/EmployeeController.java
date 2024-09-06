35be21231a90069469ab1feac336441c33fca3a3
Akhilesh Maurya <Akhilesh.Maurya@godigit.com>
2024-09-06 14:29:30 +0530

    fourth commit with service and controller with update function

--- main/java/com/example/day23SpringBoot/controller/EmployeeController.java
+++ main/java/com/example/day23SpringBoot/controller/EmployeeController.java
@@ -1,6 +1,8 @@
 package com.example.day23SpringBoot.controller;
 
+import com.example.day23SpringBoot.Exception.ResourceNotFoundException;
 import com.example.day23SpringBoot.model.EmployeeModel;
+import com.example.day23SpringBoot.repository.EmployeeRepository;
 import com.example.day23SpringBoot.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -17,9 +19,29 @@ public class EmployeeController {
     @Autowired
     public EmployeeService service;
 
+    @Autowired
+    public EmployeeRepository repository;
+
     @PostMapping("/addEmployee")
     public EmployeeModel createEmployee(@RequestBody EmployeeModel employeeModel){
         return service.createEmployee(employeeModel);
+    }
+
+    @PutMapping("/Emp/{id}")
+    public ResponseEntity<EmployeeModel> updateEmployee(@PathVariable Long id, @RequestBody EmployeeModel employeeDetails) {
+        EmployeeModel employee = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id :" + id));
+
+
+        employee.setFirstName(employeeDetails.getFirstName());
+        employee.setLastName(employeeDetails.getLastName());
+        employee.setEmail(employeeDetails.getEmail());
+        employee.setDepartment(employeeDetails.getDepartment());
+        employee.setSalary(employeeDetails.getSalary());
+
+
+        EmployeeModel updatedEmployee = repository.save(employee);
+        return ResponseEntity.ok(updatedEmployee);
     }
 
     @GetMapping("/{id}")
